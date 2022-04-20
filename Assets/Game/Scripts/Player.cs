37e9b03c50bd39795b6420bca0648a5d57f3b3c7
Veronika Kravchenko <veronika.kravchenko@unity3d.com>
2022-04-20 12:08:45 +0100

    Physics-based character controller

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private CharacterController _controller;
+    [SerializeField] private float _speed = 3.5f;
+    private float _gravity = 9.81f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(verticalInput, 0, horizontalInput);
+        Vector3 velocity = direction * _speed;
+        velocity.y -= _gravity;
+        _controller.Move(velocity * Time.deltaTime);
     }
 }
