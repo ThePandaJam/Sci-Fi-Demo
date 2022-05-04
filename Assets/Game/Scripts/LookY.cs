1ab6ccfe24ee630768ee9be590570098d8f112bb
ThePandaJam <vekrav@gmail.com>
2022-05-04 17:02:09 +0100

    minor comment and function removal

--- Assets/Game/Scripts/LookY.cs
+++ Assets/Game/Scripts/LookY.cs
@@ -5,13 +5,7 @@ using UnityEngine;
 public class LookY : MonoBehaviour
 {
     [SerializeField] private float _sensitivity = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         float mouseY = Input.GetAxis("Mouse Y");
