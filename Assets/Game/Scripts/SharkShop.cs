3d763b9b6a2941a4e7931c454f5ec2e7f7aca012
ThePandaJam <vekrav@gmail.com>
2022-05-04 16:54:46 +0100

    cleaned up shark shop code

--- Assets/Game/Scripts/SharkShop.cs
+++ Assets/Game/Scripts/SharkShop.cs
@@ -10,15 +10,6 @@ public class SharkShop : MonoBehaviour
     {
         _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
     }
-    //check for collision with player
-    //check for E key
-
-    //if player has coin
-    //  remove coin from player
-    //  update inventory display
-    //  play win sound
-    //else
-    //  debug log "you have no coin"
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
@@ -33,18 +24,14 @@ public class SharkShop : MonoBehaviour
                         player.SpendCoin();
                         _weaponSaleAudio.Play();
                         _uiManager.ToggleCoinDisplay(false);
-                        //sell weapon
                         player.EnableWeapons();
                     }
                     else
                     {
                         Debug.Log("You have no coins! Get outta here!");
                     }
-                    
                 }
             }
-            
-            
         }
     }
 }
